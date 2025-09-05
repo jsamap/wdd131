9554100f844e7677a2c4fba3c39bad55718a3950
jsamap <hsml-@hotmail.com>
2025-09-05 16:20:28 -0600

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// select the DOM elements for output
+const currentYear = document.querySelector("#currentYear");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.innerHTML = new Date().getFullYear();
+lastModified.innerHTML = `Last modification: ${new Date(document.lastModified).toLocaleString('en-US')}`;
