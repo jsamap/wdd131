7ae64d8d3dc223bb2e44234816c9fb83d739cdb9
jsamap <hsml-@hotmail.com>
2025-09-07 23:40:05 -0600

    W02 Learning Activity: Handling DOM Events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,24 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.appendChild(deleteButton);
-list.appendChild(li);
+button.addEventListener('click', function() {
+    if (input.value.trim() !== "") {
+        
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        input.value = "";
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+        });
+
+    }
+    input.focus();
+});
