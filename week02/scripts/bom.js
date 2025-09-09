32e855d70ad82af4fad041c9f555810b7b049798
jsamap <hsml-@hotmail.com>
2025-09-09 01:05:08 -0600

    W02 Assignment: Picture Album

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+
 button.addEventListener('click', function() {
     if (input.value.trim() !== "") {
         
